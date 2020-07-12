b881ea4ea7d336ac0524183391b4feda85c6d49f
gschoolpro <49395918+gschoolpro@users.noreply.github.com>
2020-07-12 13:55:44 +0900

    Add files via upload

---
+++ slope.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import PRi.GPIO as GPIO
+import time
+
+Sw520dPin = 18
+
+
+def print_message():
+    print('プログラムが動いています')
+
+
+def setup():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(Sw520dPin, GPIO.IN)
+
+
+def main():
+    print_message()
+    while True:
+        if(GPIO.input(Sw520dPin) == 0):
+            print('*  検知しました!  *\n')
+            time.sleep(1)
+        else:
+            print('*  検知できません...  *\n')
+            time.sleep(1)
+
+
+def destroy():
+    GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    setup()
+    main()
